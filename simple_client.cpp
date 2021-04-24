c3230059a3949dcf21dc564a61cec44e5463ad59
Talib Pierson <piersont@reed.edu>
2021-04-23 18:22:52 -0700

    idk
    Signed-off-by: Talib Pierson <piersont@reed.edu>

---
+++ simple_client.cpp
@@ -0,0 +1,29 @@
+#include <cstdlib>
+#include <iostream>
+
+#include "libtorrent/bencode.hpp"
+#include "libtorrent/entry.hpp"
+#include "libtorrent/session.hpp"
+#include "libtorrent/torrent_info.hpp"
+
+int main(int argc, char *argv[]) try {
+    if (argc != 2) {
+        std::cerr << "usage: ./simple_client torrent-file\n"
+                     "to stop the client, press return.\n";
+        return 1;
+    }
+
+    lt::session s;
+    lt::add_torrent_params p;
+    p.save_path = ".";
+    p.ti = std::make_shared<lt::torrent_info>(argv[1]);
+    s.add_torrent(p);
+
+    // wait for the user to end
+    char a;
+    int ret = std::scanf("%c\n", &a);
+    (void) ret;  // ignore
+    return 0;
+} catch (std::exception const &e) {
+    std::cerr << "ERROR: " << e.what() << "\n";
+}
