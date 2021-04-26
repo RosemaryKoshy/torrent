edb47023d53caedf296b9ca6cee03dafc8dab65d
Talib Pierson <piersont@reed.edu>
2021-04-25 20:32:40 -0700

    print torrent info
    Signed-off-by: Talib Pierson <piersont@reed.edu>

--- simple_client.cpp
+++ simple_client.cpp
@@ -1,5 +1,6 @@
 #include <cstdlib>
 #include <iostream>
+#include <iomanip>
 
 #include "libtorrent/bencode.hpp"
 #include "libtorrent/entry.hpp"
@@ -10,20 +11,24 @@ int main(int argc, char *argv[]) try {
     if (argc != 2) {
         std::cerr << "usage: ./simple_client torrent-file\n"
                      "to stop the client, press return.\n";
-        return 1;
+        return EXIT_FAILURE;
     }
 
     lt::session s;
     lt::add_torrent_params p;
     p.save_path = ".";
     p.ti = std::make_shared<lt::torrent_info>(argv[1]);
+
+    std::cout << "name         : " << p.ti->name() << std::endl;
+    const std::time_t creation_date = p.ti->creation_date();
+    std::cout << "creation_date: " << std::put_time(gmtime(&creation_date), "%c") << std::endl;
+    std::cout << "creator      : " << p.ti->creator() << std::endl;
+    std::cout << "comment      : " << p.ti->comment() << std::endl;
+    std::cout << "save_path    : " << p.save_path << std::endl;
+
     s.add_torrent(p);
 
-    // wait for the user to end
-    char a;
-    int ret = std::scanf("%c\n", &a);
-    (void) ret;  // ignore
-    return 0;
+    return EXIT_SUCCESS;
 } catch (std::exception const &e) {
     std::cerr << "ERROR: " << e.what() << "\n";
 }
